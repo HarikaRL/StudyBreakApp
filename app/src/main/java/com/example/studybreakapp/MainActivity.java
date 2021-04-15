504fb75f4c96d270a6b80c8e6daf1169d2139f25
Rachel Haynes <haynesrachelmarie@gmail.com>
2021-04-15 07:57:05 -0400

    home screen layout with instructions and a working start button

--- app/src/main/java/com/example/studybreakapp/MainActivity.java
+++ app/src/main/java/com/example/studybreakapp/MainActivity.java
@@ -3,11 +3,31 @@ package com.example.studybreakapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button start;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        start = findViewById(R.id.background_button);
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                movetoSelectImage();
+            }
+        });
+    }
+
+    private void movetoSelectImage() {
+        Intent intent = new Intent(MainActivity.this, selectImage.class);
+        startActivity(intent);
     }
 }
