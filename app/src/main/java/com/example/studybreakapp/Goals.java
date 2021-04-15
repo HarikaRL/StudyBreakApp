85fc3b5c66ed375a00cdc54dde21109067a3fd3a
Rachel Haynes <haynesrachelmarie@gmail.com>
2021-04-15 08:42:46 -0400

    working home and goals buttons

--- app/src/main/java/com/example/studybreakapp/Goals.java
+++ app/src/main/java/com/example/studybreakapp/Goals.java
@@ -2,13 +2,30 @@ package com.example.studybreakapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Goals extends AppCompatActivity {
+    private Button home;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_goals);
+
+        home = findViewById(R.id.home_button);
+
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                home();
+            }
+        });
+    }
+    private void home() {
+        Intent intent = new Intent(Goals.this, MainActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/studybreakapp/selectImage.java
+++ app/src/main/java/com/example/studybreakapp/selectImage.java
@@ -2,13 +2,47 @@ package com.example.studybreakapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class selectImage extends AppCompatActivity {
+    private Button home;
+    private Button goals;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_image);
+
+        home = findViewById(R.id.home_button);
+
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                home();
+            }
+        });
+
+        goals = findViewById(R.id.goals_button);
+
+        goals.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goals();
+            }
+        });
     }
+
+    private void home() {
+        Intent intent = new Intent(selectImage.this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    private void goals() {
+        Intent intent = new Intent(selectImage.this, Goals.class);
+        startActivity(intent);
+    }
+
 }
