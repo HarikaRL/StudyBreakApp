d0cbc391590059487dfe5321ac34e4696cb64ad1
kvscomputing <59271838+kvscomputing@users.noreply.github.com>
2021-04-29 07:51:42 -0400

    Paint By Numbers Initial

--- app/src/main/java/com/example/studybreakapp/PaintByNumbers.java
+++ app/src/main/java/com/example/studybreakapp/PaintByNumbers.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
@@ -20,6 +21,46 @@ import java.util.Arrays;
 
 public class PaintByNumbers extends AppCompatActivity {
 
+    int[] commonColors = {Color.rgb(51,0,0), Color.rgb(51,25,0)
+    , Color.rgb(51,51,0), Color.rgb(25,51,0), Color.rgb(0,51,0),
+    Color.rgb(0,51,25), Color.rgb(0,51,51), Color.rgb(0,25,51),
+    Color.rgb(0,0,51), Color.rgb(25,0,51), Color.rgb(51,0,51),
+    Color.rgb(51,0,25), Color.rgb(0,0,0), Color.rgb(102,0,0),
+    Color.rgb(102,51,0), Color.rgb(102,102,0), Color.rgb(51,102,0),
+    Color.rgb(0,102,0), Color.rgb(0,102,51), Color.rgb(0,102,102),
+    Color.rgb(0,51,102), Color.rgb(0,0,102), Color.rgb(51,0,102),
+    Color.rgb(102,0,102), Color.rgb(102,0,51), Color.rgb(32,32,32),
+    Color.rgb(153,0,0), Color.rgb(153,76,0), Color.rgb(153,153,0),
+    Color.rgb(76,153,0), Color.rgb(0,153,0), Color.rgb(0,153,76),
+    Color.rgb(0,153,153), Color.rgb(0,76,153), Color.rgb(0,0,153),
+    Color.rgb(76,0,153), Color.rgb(153,0,153), Color.rgb(153,0,76),
+    Color.rgb(64,64,64), Color.rgb(204,0,0), Color.rgb(204,102,0),
+    Color.rgb(204,204,0), Color.rgb(102,204,0), Color.rgb(0,204,0),
+    Color.rgb(0,204,102), Color.rgb(0,204,204), Color.rgb(0,102,204),
+    Color.rgb(0,0,204), Color.rgb(102,0,204), Color.rgb(204,0,204),
+    Color.rgb(204,0,102), Color.rgb(96,96,96), Color.rgb(255,0,0),
+    Color.rgb(255,128,0), Color.rgb(255,255,0), Color.rgb(128,255,0),
+    Color.rgb(0,255,0), Color.rgb(0,255,128), Color.rgb(0,255,255),
+    Color.rgb(0,128,255), Color.rgb(0,0,255), Color.rgb(128,0,255), Color.rgb(255,0,255),
+    Color.rgb(255,0,128), Color.rgb(128,128,128), Color.rgb(255,51,51),
+    Color.rgb(255,153,51), Color.rgb(255,255,51), Color.rgb(153,255,51),
+    Color.rgb(51,255,51), Color.rgb(51,255,153), Color.rgb(51,255,255),
+    Color.rgb(51,153,255), Color.rgb(51,51,255), Color.rgb(153,51,255),
+    Color.rgb(255,51,255), Color.rgb(255,51,153), Color.rgb(160,160,160),
+    Color.rgb(255,102,102), Color.rgb(255,178,102), Color.rgb(255,255,102),
+    Color.rgb(178,255,102), Color.rgb(102,255,102), Color.rgb(102,255,178),
+    Color.rgb(102,255,255), Color.rgb(102,178,255), Color.rgb(102,102,255),
+    Color.rgb(178,102,255), Color.rgb(255,102,255), Color.rgb(255,102,178),
+    Color.rgb(192,192,192), Color.rgb(255,153,153), Color.rgb(255,204,153),
+    Color.rgb(255,255,153), Color.rgb(204,255,153), Color.rgb(153,255,153),
+    Color.rgb(153,255,204), Color.rgb(153,255,255), Color.rgb(153,204,255),
+    Color.rgb(153,153,255), Color.rgb(204,153,255), Color.rgb(255,153,255),
+    Color.rgb(255,153,204), Color.rgb(224,224,224), Color.rgb(255,204,204),
+    Color.rgb(255,229,204), Color.rgb(255,255,204), Color.rgb(229,255,204),
+    Color.rgb(204,255,204), Color.rgb(204,255,229), Color.rgb(204,255,255),
+    Color.rgb(204,229,255), Color.rgb(204,204,255), Color.rgb(229,204,255),
+    Color.rgb(255,204,255), Color.rgb(255,204,229), Color.rgb(255,255,255)};
+
     ImageView img;
     Bitmap bit;
     int[][][] pixelGrid;
@@ -30,7 +71,7 @@ public class PaintByNumbers extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_paint_by_numbers);
         img = findViewById(R.id.ImgBeingPainted);
-        bit = BitmapFactory.decodeResource(getResources(),R.drawable.rainbowheart);
+        bit = loadBitmapFromView(img);
         pixelGrid = getPixelGrid(bit);
         bit = clearImg(bit);
         img.setImageBitmap(bit);
@@ -39,46 +80,45 @@ public class PaintByNumbers extends AppCompatActivity {
         img.getLocationOnScreen(viewCoords);
     }
 
-    public static int getScreenHeight() {
-        float screenHeight = Resources.getSystem().getDisplayMetrics().heightPixels;
-        float screenWidth = Resources.getSystem().getDisplayMetrics().widthPixels;
-
-        float aspectRatio  = screenWidth/screenHeight;
-
-        int modifiedScreenHeight = 1000;
-        return modifiedScreenHeight;
-    }
-
-    public static int getScreenWidth() {
-        float screenHeight = Resources.getSystem().getDisplayMetrics().heightPixels;
-        float screenWidth = Resources.getSystem().getDisplayMetrics().widthPixels;
-
-        float aspectRatio  = screenWidth/screenHeight;
-        int modifiedScreenHeight = 1000;
-        int modifiedScreenWidth = (int) (modifiedScreenHeight * aspectRatio);
-        return modifiedScreenWidth;
+    public static Bitmap loadBitmapFromView(View v) {
+        Bitmap b = Bitmap.createBitmap( v.getLayoutParams().width, v.getLayoutParams().height, Bitmap.Config.ARGB_8888);
+        Canvas c = new Canvas(b);
+        v.layout(0, 0, v.getLayoutParams().width, v.getLayoutParams().height);
+        v.draw(c);
+        return b;
     }
 
     public int[][][] getPixelGrid(Bitmap bitmap) {
         int height = (int)bitmap.getHeight()/30;
         int width = (int)bitmap.getWidth()/30;
-        int[][][] pixelGrid = new int[height][width][3];
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++) {
+        int[][][] pixelGrid = new int[width][height][3];
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
                 int redSum = 0;
                 int blueSum = 0;
                 int greenSum = 0;
                 for (int k = 0; k < 30; k++) {
                     for (int l = 0; l < 30; l++) {
-                        int pixel = bitmap.getPixel(30*i+k, 30*j + l);
+                        int pixel = bitmap.getPixel(30 * i + k, 30 * j + l);
                         redSum += Color.red(pixel);
                         blueSum += Color.blue(pixel);
                         greenSum += Color.green(pixel);
                     }
                 }
-                pixelGrid[i][j][0] = Math.round((float)redSum/900);
-                pixelGrid[i][j][1] = Math.round((float)greenSum/900);
-                pixelGrid[i][j][2] = Math.round((float)blueSum/900);
+                pixelGrid[i][j][0] = Math.round((float) redSum / 900);
+                pixelGrid[i][j][1] = Math.round((float) greenSum / 900);
+                pixelGrid[i][j][2] = Math.round((float) blueSum / 900);
+                int nearestColor = Color.WHITE;
+                int minDist = 1000000000;
+                for (int k = 0; k < commonColors.length; k++) {
+                    if ((int)(Math.pow(pixelGrid[i][j][0]-Color.red(commonColors[k]),2)+Math.pow(pixelGrid[i][j][1]-Color.green(commonColors[k]),2)+Math.pow(pixelGrid[i][j][2] - Color.blue(commonColors[k]),2)) < minDist) {
+                        minDist = (int)(Math.pow(pixelGrid[i][j][0]-Color.red(commonColors[k]),2)+Math.pow(pixelGrid[i][j][1]-Color.green(commonColors[k]),2)+Math.pow(pixelGrid[i][j][2] - Color.blue(commonColors[k]),2));
+                        nearestColor = commonColors[k];
+                    }
+                }
+                pixelGrid[i][j][0] = Color.red(nearestColor);
+                pixelGrid[i][j][1] = Color.green(nearestColor);
+                pixelGrid[i][j][2] = Color.blue(nearestColor);
             }
         }
         return pixelGrid;
@@ -98,6 +138,7 @@ public class PaintByNumbers extends AppCompatActivity {
         for (int i = 0; i < 30; i++) {
             for (int j = 0; j < 30; j++) {
                 bitmap.setPixel(30*x + i, 30*y + i, Color.rgb(pixelGrid[x][y][0], pixelGrid[x][y][1], pixelGrid[x][y][2]));
+                img.setImageBitmap(bitmap);
             }
         }
     }
