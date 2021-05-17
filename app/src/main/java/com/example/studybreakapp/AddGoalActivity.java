19e34f1a1923f6392609823bb0df8144ee4fad22
harika <hlingamarla@wpi.edu>
2021-05-17 16:52:09 -0400

    Finished comments

--- app/src/main/java/com/example/studybreakapp/AddGoalActivity.java
+++ app/src/main/java/com/example/studybreakapp/AddGoalActivity.java
@@ -11,6 +11,9 @@ import android.view.MenuItem;
 import android.widget.Toast;
 import android.content.Intent;
 
+/**
+ * The AddGoalActivity allows the user to add new goals by entering the goal text into a text field
+ */
 public class AddGoalActivity extends AppCompatActivity {
 
     public static final String EXTRA_TEXT =
@@ -18,17 +21,24 @@ public class AddGoalActivity extends AppCompatActivity {
 
     private EditText editText;
 
+    /**
+     * onCreate method sets the format according to activity_add_goal layout file and sets the
+     * text to the edit_text TextView in the layout file
+     * @param savedInstanceState the saved stat of the application
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_goal);
 
         editText = findViewById(R.id.edit_text);
-
-        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close);
-        setTitle("Add Goal");
     }
 
+    /**
+     * Saves a goal by checking whether the text entered by the user is empty. If so, method
+     * displays a toast message prompting the user to enter the goal. If not, method stores
+     * the text entered and sets the activity result to ok.
+     */
     private void saveGoal() {
         String text = editText.getText().toString();
 
@@ -45,6 +55,11 @@ public class AddGoalActivity extends AppCompatActivity {
 
     }
 
+    /**
+     * Creates the menu for the page
+     * @param menu the menu to add
+     * @return boolean indicating if the menu was successfully created
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater menuInflater = getMenuInflater();
@@ -52,6 +67,11 @@ public class AddGoalActivity extends AppCompatActivity {
         return super.onCreateOptionsMenu(menu);
     }
 
+    /**
+     * Calls the saveGoal method when the check mark in the menu is clicked
+     * @param item the menu item that indicates the goal should be saved, in this case the check mark
+     * @return true if the goal was successfully saved, otherwise defaults to the superclass method
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch(item.getItemId()) {
